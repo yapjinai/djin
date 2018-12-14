21e025dabf5ee1ffba905894936df0ed1928e9d6
yapjinai <jinai.jyap@gmail.com>
2018-12-14 03:06:01 +0100

    copied static build folder from main frontend project

---
+++ precache-manifest.62a68023767e7a0216de30067cd036d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d70af6e01623bc18882",
+    "url": "/static/js/main.4d70af6e.chunk.js"
+  },
+  {
+    "revision": "d73340831af562ae9434",
+    "url": "/static/js/1.d7334083.chunk.js"
+  },
+  {
+    "revision": "4d70af6e01623bc18882",
+    "url": "/static/css/main.368eb956.chunk.css"
+  },
+  {
+    "revision": "8764d4c606f82af9c1b977681c19968f",
+    "url": "/index.html"
+  }
+];
